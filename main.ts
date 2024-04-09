77ab033d219100e887e4b123b693e182c8100b1d
jaewonE <importjaewonE@gmail.com>
2024-04-09 16:52:19 +0900

    Version 1.0.0

--- main.ts
+++ main.ts
@@ -1,134 +1,183 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as fs from "fs";
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FindProjectNoteSetting {
+	HocTag: string;
+	MocTag: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FindProjectNoteSetting = {
+	HocTag: "HOC",
+	MocTag: "MOC",
+};
+
+enum TagType {
+	HOC,
+	MOC,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FindProjectNotePlugin extends Plugin {
+	settings: FindProjectNoteSetting;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: "open-project-note",
+			name: "Open project note",
+			callback: () => this.openFindingNote(TagType.HOC),
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: "open-moc-note",
+			name: "Open map of content note",
+			callback: () => this.openFindingNote(TagType.MOC),
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		this.addSettingTab(new FindProjectNoteSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async openFindingNote(type: TagType) {
+		const activeFile = this.app.workspace.getActiveFile();
+		let curFileHasType = false;
+		if (!activeFile) {
+			return; // 활성 파일이 없는 경우 종료
+		}
+
+		const pathParts = activeFile.path.split("/");
+
+		// @ts-ignore
+		const __dirname = this.app.vault.adapter.basePath;
+
+		for (let i = pathParts.length - 1; i >= 0; i--) {
+			const filename = `${pathParts[i - 1]}.md`;
+			const projectNote =
+				pathParts.slice(0, i).join("/") + `/${filename}`;
+			const projectNotePath = `${__dirname}/${projectNote}`;
+			// new Notice(`projectNote: ${projectNotePath} exist: ${fs.existsSync(projectNotePath)}`,5000);
+
+			if (fs.existsSync(projectNotePath)) {
+				const isFolderNote = await this.checkForTag(
+					projectNotePath,
+					type
+				);
+				// new Notice(`isFolderNote: ${isFolderNote}`, 5000);
+
+				if (
+					pathParts.length - 1 === i &&
+					filename === pathParts[pathParts.length - 1] &&
+					isFolderNote
+				) {
+					// new Notice(`Current file is project note: ${projectNote}`,5000);
+					curFileHasType = true;
+					continue;
+				}
+
+				if (isFolderNote) {
+					// new Notice(`Opening project note: ${projectNote}`, 5000);
+					this.app.workspace.openLinkText(projectNote, "/", false);
+					return;
+				}
+			}
+		}
+
+		if (!curFileHasType) {
+			const readmePath = `${__dirname}/README.md`;
+			if (fs.existsSync(readmePath)) {
+				this.app.workspace.openLinkText(readmePath, "/", false);
+			} else {
+				new Notice(
+					`Project note with ${this.settings.HocTag} tag not found in the tags property.`,
+					3000
+				);
+			}
+		}
+	}
+
+	async checkForTag(filePath: string, type: TagType): Promise<boolean> {
+		try {
+			const fileContent = await fs.promises.readFile(filePath, "utf-8");
+			// 파일 내용에서 첫 번째 "---"로 시작하는 블록을 찾음
+			const frontMatterBlock = fileContent.match(/^---\n([\s\S]*?)\n---/);
+			if (!frontMatterBlock) {
+				return false;
+			}
+
+			if (frontMatterBlock) {
+				// YAML 형태의 문자열에서 "tags:" 이후의 내용을 추출
+				const tagsMatch =
+					frontMatterBlock[1].match(/tags:\n(.*?)\n(?=\w)/s);
+				if (tagsMatch) {
+					// "tags" 섹션에서 각 태그를 배열로 변환
+					const tags = tagsMatch[1]
+						.split("\n")
+						.map((tag) => tag.trim().replace(/^- /, ""));
+					// new Notice(`tags: ${tags}`, 5000);
+
+					switch (type) {
+						case TagType.HOC:
+							return tags.includes(this.settings.HocTag);
+						case TagType.MOC:
+							return tags.includes(this.settings.MocTag);
+						default:
+							return false;
+					}
+				}
+			}
+		} catch (error) {
+			console.error(`Error reading file: ${error}`);
+		}
+		return false;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class FindProjectNoteSettingTab extends PluginSettingTab {
+	private readonly plugin: FindProjectNotePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FindProjectNotePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
-	display(): void {
-		const {containerEl} = this;
-
+	public display(): void {
+		const { containerEl } = this;
 		containerEl.empty();
+		containerEl.createEl("h2", { text: "Find project Note" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Project Note Tag")
+			.setDesc("The tag to search for in the project note.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your tag without #")
+					.setValue(this.plugin.settings.HocTag)
+					.onChange(async (value) => {
+						this.plugin.settings.HocTag = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Map of content Note Tag")
+			.setDesc("The tag to search for map of content note.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your tag without #")
+					.setValue(this.plugin.settings.MocTag)
+					.onChange(async (value) => {
+						this.plugin.settings.MocTag = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
